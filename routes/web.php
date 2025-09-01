84ba634204e2ea4e822793b8649a82c2d8901b66
Sonia Castro <desarrolloafs2@gmail.com>
2025-09-01 14:50:31 +0100

    isForTesting' => false, // Relleno automático para pruebas

--- routes/web.php
+++ routes/web.php
@@ -29,7 +29,7 @@ Route::get('{type}', function ($type) {
 
     return view($type, [
         'type' => $type,
-        'isForTesting' => true, // Relleno automático para pruebas
+        'isForTesting' => false, // Relleno automático para pruebas
     ]);
 });
 
